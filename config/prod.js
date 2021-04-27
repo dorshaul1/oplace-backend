313044abfd55f2d0c539d9b4678418f4f7d1dbbb
dorshaul1 <dor.asti@gmail.com>
2021-04-27 15:44:31 +0300

    sdc

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://oplace-admin:oplace123@cluster0.dnicy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
--- services/db.service.js
+++ services/db.service.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 // Database Name
-const dbName = 'oplace_db'
+const dbName = 'oplace'
 
 var dbConn = null
 
